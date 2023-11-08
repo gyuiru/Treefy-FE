3b69f5357cb3d462d1ae086e186759f73ff2be1b
gyuiru <gyuiru8@gmail.com>
2023-11-08 13:46:39 +0900

    feat: piniaインストール
    VuexよりCompositionApiとよく総合されており、Piniaを利用する
    
    #2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app') ;
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(router).use(pinia).mount('#app');
 
