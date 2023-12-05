f2e97e43164fbdf705b3a122461a2528c41b6a17
gyuiru <gyuiru8@gmail.com>
2023-12-05 10:17:43 +0900

    feat: postデータをサーバーに送信
    Axiosを通じてpostデーターをサーバーに送信
    
    #7

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,33 @@
 import { defineStore } from 'pinia'
 // import asdf from 'asdf'; // 데이터 가져오기 (json같은)
+import axios, { Axios } from 'axios';
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 export const useStore = defineStore('main', {
 
-  state: () => {
-      return {
+  state: () => ({
+    post: {
+      title: '',
+      content: ''
+    } as Post
           // asdf, // 데이터 넣기 (이름이 같기에 : 안쓰고 하나로 씀);
+          
+  }),
+  actions: {  //actions
+    async setPost(post: Post) {
+      this.post = post;
+      try {
+        const response = await axios.post('/api/posts', post);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
       }
-  }
-
-  //actions
+    },
+  },
 
   //getters
-})
+});
